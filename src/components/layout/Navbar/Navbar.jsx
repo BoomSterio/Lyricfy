4168d98b43a5468be432a5fcaa19b09b69b4dae8
Maksym <max.sharinov@gmail.com>
2021-03-28 00:53:03 +0200

    Hooks

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className={'navbar navbar-dark bg-dark mb-5'}>
-      <span style={{color:'lightgreen'}} className="navbar-brand md-0 h1 mx-auto">Lyricfy</span>
+    <nav className={'navbar navbar-dark bg-dark mb-5 sticky-top'}>
+      <Link to={'/'} style={{color:'lightgreen'}} className="navbar-brand md-0 h1 mx-auto">Lyricfy</Link>
     </nav>
   )
 }
--- src/components/tracks/Search/Search.jsx
+++ src/components/tracks/Search/Search.jsx
@@ -1,68 +1,74 @@
-import React, { Component } from 'react'
-import { Consumer } from '../../../context'
+import React, { useContext, useState } from 'react'
+import { Context } from '../../../context'
 import axios from 'axios'
 
-class Search extends Component {
-  state = {
-    trackTitle: '',
-  }
+const ALL = '_track_artist'
+const TRACK = '_track'
+const ARTIST = '_artist'
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value })
+const Search = () => {
+  const [query, setQuery] = useState('')
+  const [searchBy, setSearchBy] = useState(ALL)
 
-  findTrack = (dispatch, e) => {
+  const setGlobalState = useContext(Context)[1]
+
+  const onChange = e => setQuery(e.target.value)
+
+  const onSelect = e => setSearchBy(e.target.value)
+
+  const findTrack = (e) => {
     e.preventDefault()
 
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+        `https://thingproxy.freeboard.io/fetch/https://api.musixmatch.com/ws/1.1/track.search?q${searchBy}=${query}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(res => {
-        dispatch({
-          type: 'SEARCH_TRACKS',
-          payload: res.data.message.body.track_list,
+        if(res.data.message.body.track_list.length === 0) alert('Nothing was found')
+
+        setGlobalState({
+          heading: 'Search Results',
+          trackList: res.data.message.body.track_list,
         })
-        this.setState({ trackTitle: '' })
+        setQuery('')
       })
       .catch(err => console.error(err))
   }
 
-  render() {
-    return (
-      <Consumer>
-        {value => {
-          const { dispatch } = value
-          return (
-            <div className={'card card-body mb-4 p-4'}>
-              <h1 className="display-4 text-center">
-                <i className="fas fa-music" /> Search for a Song
-              </h1>
-              <p className="lead text-center">Get lyrics for any Song</p>
-              <form onSubmit={this.findTrack.bind(this, dispatch)}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder={'Song title...'}
-                    name={'trackTitle'}
-                    value={this.state.trackTitle}
-                    onChange={this.onChange}
-                  />
-                  <button
-                    className="btn btn-primary btn-lg mb-5 mt-2 w-100"
-                    type={'submit'}
-                    disabled={!this.state.trackTitle}
-                  >
-                    Get Track Lyrics
-                  </button>
-                </div>
-
-              </form>
-            </div>
-          )
-        }}
-      </Consumer>
-    )
-  }
+  return (
+    <div className={'card card-body mb-4 p-4'}>
+      <h1 className="display-4 text-center">
+        <i className="fas fa-music" /> Search for a Song
+      </h1>
+      <p className="lead text-center">Get lyrics for any Song</p>
+      <form onSubmit={findTrack}>
+        <div className="form-group">
+          <div className={'d-md-flex'}>
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              style={{marginRight: '10px'}}
+              placeholder={'Song title...'}
+              value={query}
+              onChange={onChange}
+            />
+            <select defaultChecked={searchBy} onChange={onSelect} className="form-select" style={{width: '100px'}}>
+              <option value={ALL}>All</option>
+              <option value={TRACK}>Track</option>
+              <option value={ARTIST}>Artist</option>
+            </select>
+          </div>
+          <button
+            className="btn btn-primary btn-lg mb-5 mt-2 w-100"
+            type={'submit'}
+            disabled={!query}
+          >
+            Get Track Lyrics
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Search
